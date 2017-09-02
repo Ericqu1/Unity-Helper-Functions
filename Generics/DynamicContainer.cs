bf16cd4a294ed5b4e46f69d893e6bab5c4c303c1
Eric Qu <EricQu96@gmail.com>
2017-09-02 15:44:36 -0700

    This helps group all dynamically instantiated objects into one hierarchy location named _Dynamic

---
+++ Generics/DynamicContainer.cs
@@ -0,0 +1,15 @@
+﻿namespace Generics
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class DynamicContainer : GenericSingletonClass<DynamicContainer> 
+	{
+		public override void Awake ()
+		{
+			base.Awake ();
+			gameObject.name = "_Dynamic";
+		}
+	}
+}
