86049eac40f744367be17b128bac19d7269da2d6
Eric Qu <EricQu96@gmail.com>
2017-09-02 15:59:23 -0700

    Added a TotalScale finding function

---
+++ MathFuncs/TransformUtilities.cs
@@ -0,0 +1,10 @@
+﻿namespace MathFuncs
+{
+	using UnityEngine;
+	
+	public static Vector3 TotalScale(this Transform t)
+		{
+			Vector3 parentScale = (t.parent) ? t.parent.TotalScale () : Vector3.one;
+			return Vector3.Scale(t.localScale, parentScale);
+		}
+}
