e1e85ede0b5e1ba658a151e22d6db90dc50b82ae
Eric Qu <eric@escalitygames.com>
2018-03-06 13:46:19 -0800

    Create ResourcesUtilities.cs

---
+++ Utilities/ResourcesUtilities.cs
@@ -0,0 +1,16 @@
+namespace Utilities
+{
+	using UnityEngine;
+
+	public static class ResourcesUtilities
+	{
+		public static string StreamingAssetsFilePath(string fileName)
+		{
+			#if UNITY_ANDROID && !UNITY_EDITOR
+			return "jar:file://" + Application.dataPath + "!/assets/" + fileName;
+			#else
+			return System.IO.Path.Combine (Application.streamingAssetsPath, fileName);
+			#endif
+		}
+	}
+}
