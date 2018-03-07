f6dcb98509fcb8aad897367a7e38ae0b47631f03
Eric Qu <eric@escalitygames.com>
2018-03-06 16:42:59 -0800

    Create ColorUtilities.cs

---
+++ Utilities/ColorUtilities.cs
@@ -0,0 +1,12 @@
+namespace Utilities
+{
+	using UnityEngine;
+
+	public static class ColorUtilities
+	{
+		public static Color ChangeTransparency(this Color color, float alpha)
+		{
+			return new Color (color.r, color.g, color.b, alpha);
+		}
+	}
+}
