cb2cb90d04a206f43465881c7cbe9c67b59db54c
Eric Qu <EricQu96@gmail.com>
2018-06-11 22:50:18 -0700

    Adding UnityEvent extension

---
+++ UnityExtensions/UnityEventExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace UnityEngine.Events.Extensions
+{
+    // Useful for serializing a UnityEvent that passes an int argument
+    [System.Serializable]
+    public class UnityEvent_int : UnityEvent<int> { }
+}
